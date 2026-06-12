96197c7c32ff9a6e2c37208229456cdcd8b14c65
rayhan108 <raihanshorker56@gmail.com>
2026-06-12 14:50:08 +0600

    practice user input on socket prog

--- MyClient.java
+++ MyClient.java
@@ -1,18 +1,56 @@
+// import java.io.DataOutputStream;
+// import java.net.Socket;
+
+// public class MyClient {
+//     public static void main(String[] args) {
+//         try {
+//             Socket s = new Socket("localhost", 5003);
+//             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+//             dout.writeUTF("Hello Server I am from client");
+//             dout.flush();
+//             dout.close();
+//             s.close();
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+
+//     }
+// }
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyClient {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         try {
             Socket s = new Socket("localhost", 5003);
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF("Hello Server I am from client");
+
+            System.out.print("Enter Student ID: ");
+            int id = sc.nextInt();
+            sc.nextLine(); 
+            System.out.print("Enter Student Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter Result (GPA): ");
+            double gpa = sc.nextDouble();
+
+          
+            String info = id + "," + name + "," + gpa;
+
+           
+            dout.writeUTF(info);
             dout.flush();
+
+            System.out.println("Student info sent successfully!");
+
             dout.close();
             s.close();
+            sc.close();
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Error: " + e);
         }
-
     }
 }
--- MyServer.java
+++ MyServer.java
@@ -1,3 +1,28 @@
+// import java.io.DataInputStream;
+// import java.net.ServerSocket;
+// import java.net.Socket;
+
+// public class MyServer {
+//     public static void main(String[] args) {
+//         try {
+//             // port open
+//             ServerSocket ss = new ServerSocket(5003);
+//             System.out.println("server is waiting on port 5003");
+//             // client req accept
+//             Socket s = ss.accept();
+//             System.out.println("client connected!");
+
+//             DataInputStream dis = new DataInputStream(s.getInputStream());
+//             String str = (String) dis.readUTF();
+
+//             System.out.println("message from cleint " + str);
+//             ss.close();
+
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+//     }
+// }
 import java.io.DataInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -5,21 +30,27 @@ import java.net.Socket;
 public class MyServer {
     public static void main(String[] args) {
         try {
-            // port open
             ServerSocket ss = new ServerSocket(5003);
-            System.out.println("server is waiting on port 5003");
-            // client req accept
+            System.out.println("Server is waiting for data...");
+
             Socket s = ss.accept();
-            System.out.println("client connected!");
-
             DataInputStream dis = new DataInputStream(s.getInputStream());
-            String str = (String) dis.readUTF();
 
-            System.out.println("message from cleint " + str);
+  
+            String receivedData = dis.readUTF();
+
+  
+            String[] dataArray = receivedData.split(",");
+
+       
+            System.out.println("\n--- Received Student Data ---");
+            System.out.println("ID: " + dataArray[0]);
+            System.out.println("Name: " + dataArray[1]);
+            System.out.println("GPA: " + dataArray[2]);
+
             ss.close();
-
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Error: " + e);
         }
     }
 }
